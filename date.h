9d016ec8298691bc8ff75201891c41711f334989
和骁扬 <hexiaoya20@mails.tsinghua.edu.cn>
2022-02-22 21:21:18 +0800

    add m_ for private members in class date

--- date.h
+++ date.h
@@ -8,12 +8,12 @@ public:
     Date(int, int, int, int, int, int);
     bool operator < (const Date&);   // 比较时间的先后："<"——先发生
 private:
-    int sec;   // 秒，正常范围从 0 到 59，但允许至 61
-    int min;   // 分，范围从 0 到 59
-    int hour;  // 小时，范围从 0 到 23
-    int day;   // 天，范围从 1 到 31
-    int mon;   // 月，范围从 1 到 12
-    int year;  // 年
+    int m_sec;   // 秒，正常范围从 0 到 59，但允许至 61
+    int m_min;   // 分，范围从 0 到 59
+    int m_hour;  // 小时，范围从 0 到 23
+    int m_day;   // 天，范围从 1 到 31
+    int m_mon;   // 月，范围从 1 到 12
+    int m_year;  // 年
 };
 
 #endif // DATE_H
