8ee6845ac62c6c56e469df478726411e35461ab9
和骁扬 <hexiaoya20@mails.tsinghua.edu.cn>
2022-02-22 21:29:47 +0800

    add m_ for private members in date.cpp

--- date.cpp
+++ date.cpp
@@ -1,27 +1,27 @@
 #include "date.h"
 
 Date::Date(int s, int mi, int h, int d, int mo, int y):
-    sec(s), min(mi), hour(h), day(d), mon(mo), year(y)
+    m_sec(s), m_min(mi), m_hour(h), m_day(d), m_mon(mo), m_year(y)
 {}
 
 bool Date::operator < (const Date& b)   // 没想到更好的方法，打扰了
 {
-    if(this->year < b.year)
+    if(this->m_year < b.m_year)
         return true;
-    else if(this->year == b.year){
-        if(this->mon < b.mon)
+    else if(this->m_year == b.m_year){
+        if(this->m_mon < b.m_mon)
             return true;
-        else if(this->mon == b.mon){
-            if(this->day < b.day)
+        else if(this->m_mon == b.m_mon){
+            if(this->m_day < b.m_day)
                 return true;
-            else if(this->day == b.day){
-                if(this->hour < b.hour)
+            else if(this->m_day == b.m_day){
+                if(this->m_hour < b.m_hour)
                     return true;
-                else if(this->hour == b.hour){
-                    if(this->min < b.min)
+                else if(this->m_hour == b.m_hour){
+                    if(this->m_min < b.m_min)
                         return true;
-                    else if(this->min == b.min){
-                        if(this->sec < b.sec)
+                    else if(this->m_min == b.m_min){
+                        if(this->m_sec < b.m_sec)
                             return true;
                     }
                 }
