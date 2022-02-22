ed88ab8dec85bef36f244ba04d654755df119522
GiddaZhang <zhang-jd20@mails.tsinghua.edu.cn>
2022-02-22 19:01:39 +0800

    在main.cpp里添加测试

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include "mainwindow.h"
-#include <QApplication>
 #include "ddl.h"
+#include <QApplication>
 #include <QDebug>
 
 int main(int argc, char *argv[])
@@ -9,8 +9,14 @@ int main(int argc, char *argv[])
     MainWindow w;
     w.show();
 
+//    void test();
+//    test();
+    return a.exec();
+}
+
+//void test()
+//{
 //    Date d_1(0,0,0,1,1,2002), d_2(0,0,0,1,1,2003);
 //    DDL D_1(d_1, "Do homework"), D_2(d_2, "Exercise");
 //    qDebug() << (D_1 < D_2);
-    return a.exec();
-}
+//}
