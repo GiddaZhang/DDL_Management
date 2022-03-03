c0e80d3346e3f9b8d06d2cc367c39c56cea521ae
GiddaZhang <zhang-jd20@mails.tsinghua.edu.cn>
2022-03-03 16:43:07 +0800

    Add Get func for outside readonly visit

--- description.cpp
+++ description.cpp
@@ -8,7 +8,7 @@ Description::Description(QString description):
     m_timeStamp = QDateTime::currentDateTime();
 }
 
-Description::Description(QString description, QDateTime dateTime):
+Description::Description(QString description, const QDateTime& dateTime):
     TimeStamp(m_timeStamp), Note(m_note)
 {
     m_note = description;
@@ -19,3 +19,10 @@ void Description::Change(const QString& description){
     m_note = description;
 }
 
+QDateTime Description::GetStamp(){
+    return this->m_timeStamp;
+}
+
+QString Description::GetNote(){
+    return this->m_note;
+}
--- description.h
+++ description.h
@@ -9,16 +9,22 @@ using namespace  std;
 
 class Description{
 public:
-    Description(QString);           //默认构造函数，用当前时间
-    Description(QString, QDateTime);//其它构造函数，不用当前时间
-    void Change(const QString&);    //修改
-    Description& operator=(const Description& description){
+    Description(QString);                   //默认构造函数，用当前时间
+    Description(QString, const QDateTime&); //其它构造函数，不用当前时间
+    void Change(const QString&);            //修改
+    Description& operator=(const Description& description)
+    {
+        this->m_note = description.Note;
+        this->m_timeStamp = description.TimeStamp;
         return *this;
     }
 
     //常引用只读
     const QDateTime& TimeStamp;
     const QString& Note;
+    // 常引用只读有时候不好使
+    QDateTime GetStamp();
+    QString GetNote();
 
 private:
     QDateTime m_timeStamp;
